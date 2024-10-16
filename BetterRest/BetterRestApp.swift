3d41c60f8840c32157830a251b8cd4807d733291
Timothy Williams <Timothy.Williams@stu.glasgow.kyschools.us>
2024-10-16 08:57:57 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Student on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
